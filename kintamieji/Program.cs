db48def44090df69c2fc94ae173a4b8da095b435
Laura Krau <laura.krau@gmail.com>
2023-02-08 20:28:11 +0200

    kintamuju paskaita

---
+++ kintamieji/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace kintamieji
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; /*Lietuviskos raides*/
+            Console.WriteLine("Labas Pasauli");
+
+            // vienos eilutes komentaras 
+            /* daugiau nei 
+             * vienos eilutes komentaras */
+
+            string vardas = "Ieva ";
+            string pavarde = "Pertrone";
+            int amžius = 25;
+            float ugis = 1.73f;
+            int svoris = 70;
+            char simobis = '&';
+            bool tiesa = true;
+            double ilgasSkaicius = 3.1467865678768346;
+
+            svoris = 67;
+
+        }
+    }
+}
