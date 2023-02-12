ee62bd8395f4848f891098568e5d1ef28cc86e23
Laura Krau <laura.krau@gmail.com>
2023-02-12 13:16:35 +0200

    Kazka pridirbau ir dabar pandau pataisyti lol

---
+++ uzduotiss08/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace uzduotiss08
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           /* Išveskite į ekraną dviejų skaičių sumą, skirtumą, sandaugą ir dalmenį,
+            kiekvieną kartą nurodant atliekamą veiksmą ir šių skaičių reikšmes. Pvz.: 4 + 5 = 9
+            4 - 5 = -1 */ 
+
+
+
+        }
+    }
+}
