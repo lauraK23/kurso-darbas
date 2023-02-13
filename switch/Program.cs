eda00e50b0c4741fa8703e107c68e474785a7a5b
Laura Krau <laura.krau@gmail.com>
2023-02-13 19:35:15 +0200

    switch demo

---
+++ switch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace @switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string reiksme = "Katė";
+            //switch (reiksme)
+
+            switch ("Šuo")
+            {
+                case "šuo":
+                    Console.WriteLine("neteisingas");
+                    break;
+                case "Šuo":
+                    Console.WriteLine("teisingas");
+                    break;
+
+            }
+        }
+    }
+}
